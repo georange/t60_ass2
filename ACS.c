5492680a339a3ced7c1112e1cd3efbb920d2373e
georange <georgeblub@gmail.com>
2018-10-29 15:11:21 -0700

    Add files via upload

--- ACS.c
+++ ACS.c
@@ -155,6 +155,10 @@ void set_up_customers(char* to_read) {
 
 	// parse first line for total number of customers
 	total = atoi(contents[0]);
+	if (total < 0) {
+		prinf("Error: invalid value in input file.");
+		exit(1);
+	}
 
 	// add content to all_customers
 	int j = 0;
@@ -175,6 +179,11 @@ void set_up_customers(char* to_read) {
 
 		all_customers[j] = temp;
 		j++;
+		
+		if (temp->id<0 || temp->class<0 || temp->arrival_time<0 || temp->service_time < 0) {
+			prinf("Error: invalid value in input file.");
+			exit(1);
+		}
 
 		//printf("%d %d %f %f\n",temp->id, temp->class, temp->arrival_time, temp->service_time);
 
